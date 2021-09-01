1b0b6fd4ff25bd416204254ae3981e98d388e337
Ntd Chnn <notdakon.may@gmail.com>
2021-09-01 15:32:25 +0700

    finish2

--- 63010480_2.py
+++ 63010480_2.py
@@ -2,33 +2,23 @@ class Queue:
 
     def __init__(self):
         self.q = []
-        self.dq = []
+        self.index = 0
 
-    def enQ(self, value):
+    def ENq(self, value):
         self.q.append(value)
-        print(*self.q,sep=', ')
+
+    def ESq(self, value):
+        self.q.insert(self.index,value)
+        self.index += 1
 
     def deQ(self):
         if len(self.q) == 0:
             print('Empty')
         else:
-            print(f'{self.q[0]} <- ',end='')
-            self.dq.append(self.q[0])
+            print(int(self.q[0]))
             self.q.pop(0)
-            if len(self.q) == 0:
-                print('Empty')
-            else: print(*self.q,sep=', ')
-
-    def end(self):
-        if len(self.dq) == 0:
-            print('Empty',end='')
-        else:
-            print(*self.dq, sep=', ',end='')
-        print(' : ',end='')
-        if len(self.q) == 0:
-            print('Empty')
-        else:
-            print(*self.q, sep=', ',end='')
+        if self.index > 0:
+            self.index -= 1
 
 
 ip = [e for e in input("Enter Input : ").split(',')]
@@ -40,6 +30,7 @@ for i in ip:
         q.deQ()
     else:
         command, value = i.split(' ')
-        q.enQ(value)
-
-q.end()
+        if command == 'EN':
+            q.ENq(value)
+        else:
+            q.ESq(value)
